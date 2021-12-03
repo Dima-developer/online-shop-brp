d085701bc9f0d95bc0231e6e584380a5b3bc82be
Dmytro Petrenko <petrenko.d28@gmail.com>
2021-12-03 23:29:10 +0200

    add slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBlock from "./containers/NavBlock";
+import Slider from "./components/Slider";
 
 
 const App: React.FC = () => {
   return (
     <div >
       <NavBlock />
+      <Slider />
     </div>
   );
 }
